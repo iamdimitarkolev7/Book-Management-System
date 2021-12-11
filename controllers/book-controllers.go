b8a127159819fd777d1c281508d98fd94967c398
Dimitar Kolev <31987284+iamdimitarkolev7@users.noreply.github.com>
2021-12-11 18:14:42 +0200

    Finish the project
    Adding logic for update and delete a book from the database

--- controllers/book-controllers.go
+++ controllers/book-controllers.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"book-management-system/models"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -70,6 +71,41 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(payload)
 }
 
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var book models.Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	createBook(book)
+	json.NewEncoder(w).Encode(&book)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var updatedBook models.Book
+	_ = json.NewDecoder(r.Body).Decode(&updatedBook)
+	var params = mux.Vars(r)
+	updateBook(params["id"], updatedBook)
+	json.NewEncoder(w).Encode(&updatedBook)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	params := mux.Vars(r)
+	deleteBook(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
 func getAllBooks() []primitive.M {
 	curr, err := collection.Find(context.Background(), bson.D{{}})
 
@@ -110,4 +146,42 @@ func getBookById(bookId string) primitive.M {
 	}
 
 	return result
+}
+
+func createBook(book models.Book) {
+	createdBook, err := collection.InsertOne(context.Background(), book)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("A new book is successfully created with id: ", createdBook.InsertedID)
+}
+
+func updateBook(bookId string, updatedBook models.Book) {
+	id, _ := primitive.ObjectIDFromHex(bookId)
+	filter := bson.M{"_id": id}
+
+	update := bson.M{"$set": bson.M{"title": updatedBook.Title, "author": updatedBook.Author}}
+
+	_, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully updated book")
+}
+
+func deleteBook(bookId string) {
+	id, _ := primitive.ObjectIDFromHex(bookId)
+	bookToDelete := bson.M{"_id": id}
+
+	_, err := collection.DeleteOne(context.Background(), bookToDelete)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully deleted a book")
 }
--- routes/book-router.go
+++ routes/book-router.go
@@ -12,9 +12,9 @@ func Router() *mux.Router {
 
 	router.HandleFunc("/api/books", controllers.GetAllBooks).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/books/{id}", controllers.GetBookById).Methods("GET", "OPTIONS")
-	// router.HandleFunc("/api/books/create", controllers.CreateBook).Methods("POST", "OPTIONS")
-	// router.HandleFunc("/api/books/update/{id}", controllers.UpdateBook).Methods("PUT", "OPTIONS")
-	// router.HandleFunc("/api/books/delete/{id}", controllers.DeleteBook).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/books/create", controllers.CreateBook).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/books/update/{id}", controllers.UpdateBook).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/books/delete/{id}", controllers.DeleteBook).Methods("DELETE", "OPTIONS")
 
 	return router
 }
